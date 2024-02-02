b28fbf28d61bf756cf84d91e236306a60ead9a1d
Caio-Kaefer <kaefer.caio@gmail.com>
2024-02-02 20:47:16 -0300

    first commit

---
+++ initializers/database.go
@@ -0,0 +1,11 @@
+// package initializers
+
+// import (
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// func ConnecttoDB() {
+// 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// }
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Caio-Kaefer/go-crud/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
